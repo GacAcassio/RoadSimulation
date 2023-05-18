cb3238287429d8f80fbeb07845e087b20e89a442
Gabriel Acassio <100379848+GacAcassio@users.noreply.github.com>
2023-05-17 23:59:56 -0400

    Update structs.h

--- structs.h
+++ structs.h
@@ -1,26 +1,26 @@
-#indef STRUCTS_H
+#ifndef STRUCTS_H
 #define STRUCTS_H
 //Cria um tipo abstrato de dados para Veículo
-typedef struct
-{
-  int tipo, colisao, preferencial;
-  double tempo;
-}Veiculo;
+  typedef struct
+  {
+    int tipo, colisao, preferencial;
+    double tempo;
+  }Veiculo;
 
-//Cria uma abstração de dados 
-typedef struct
-{
-  Veiculo veiculos[600];
-  int inicio, fim, max;
-  int quantCarros, quantPreferenciais, quantOnibus;
-  //Booleanos para facilitar a detecção de veículos prefenciais e se a pista ainda tem(ou terá) carros
-  int prefenciais, abilitada;
-}Via;
+  //Cria uma abstração de dados 
+  typedef struct
+  {
+    Veiculo veiculos[600];
+    int inicio, fim, max;
+    int quantCarros, quantPreferenciais, quantOnibus;
+    //Booleanos para facilitar a detecção de veículos prefenciais e se a pista ainda tem(ou terá) carros
+    int prefenciais, abilitada;
+  }Via;
 
-//Cria uma abstração para rotatória com 3 vias
-typedef struct
-{
-  Via vias[3];
-}Rotatoria;
+  //Cria uma abstração para rotatória com 3 vias
+  typedef struct
+  {
+    Via vias[3];
+  }Rotatoria;
 
 #endif

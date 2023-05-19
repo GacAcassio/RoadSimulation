4dd66d526855b205bc64c200932f2c469256a85f
Gabriel Acassio <100379848+GacAcassio@users.noreply.github.com>
2023-05-18 23:36:44 -0400

    Update main.c

--- main.c
+++ main.c
@@ -7,28 +7,6 @@
 #define variacaoVeiculos 100
 #define numeroTestes 1000
 #define tempoSemaforo 15
-//Cria um tipo abstrato de dados para Veículo
-typedef struct
-{
-  int tipo, colisao, preferencial;
-  double tempo;
-}Veiculo;
-
-//Cria uma abstração de dados 
-typedef struct
-{
-  Veiculo veiculos[600];
-  int inicio, fim, max;
-  int quantCarros, quantPreferenciais, quantOnibus;
-  //Booleanos para facilitar a detecção de veículos prefenciais e se a pista ainda tem(ou terá) carros
-  int prefenciais, abilitada;
-}Via;
-
-//Cria uma abstração para rotatória com 3 vias
-typedef struct
-{
-  Via vias[3];
-}Rotatoria;
 
 //Vetor global para guardar resultados
 float tabela[numeroTestes][4];
